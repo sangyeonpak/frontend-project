9789707fcc6622624f900225ef67f726e24e8845
Sangyeon Pak <sangyeonpak1994@gmail.com>
2022-11-21 16:47:37 -0500

    I like it

---
+++ shelf.js
@@ -0,0 +1,11 @@
+let $addRow = $('#addRow');
+let $container = $('.container');
+let $row = $('<div></div>').addClass('row');
+let $colSM = $('<div></div>').addClass('col-sm');
+let $row2 = $('<div></div>').addClass('row');
+
+function addRow(){
+  $addRow.click(()=>{
+    $container.append()
+  });
+}
