eee36eea3a85df6d0189f9e6c422b11189abb28e
Sangyeon Pak <sangyeonpak1994@gmail.com>
2022-11-21 22:35:46 -0500

    Can't seem to get Bootstrap modal element to include text. I'm gonna make it a search bar to add albums

--- shelf.js
+++ shelf.js
@@ -1,11 +1,34 @@
 let $addRow = $('#addRow');
 let $container = $('.container');
-let $row = $('<div></div>').addClass('row');
-let $colSM = $('<div></div>').addClass('col-sm');
-let $row2 = $('<div></div>').addClass('row');
+let $addAlbumBtn = $('#addAlbumBtn');
+
 
 function addRow(){
   $addRow.click(()=>{
-    $container.append()
+    $addRow.detach();
+    let $rowActual = $('<div></div>').addClass('row').attr('id', 'addedRow');
+    $container.append($rowActual);
+    for (var i = 0; i < 3; i++){
+      let $column = $('<div></div>').addClass('col');
+      let $rowInner = $('<div></div>').addClass('row');
+      let $border = $('<span></span>').addClass('border');
+      let $rowUnderBorder = $('<div></div>').addClass('row');
+      let $columnImg = $('<div></div>').addClass('col');
+      let $placeholder = $('<img>').attr('src', 'images/placeholder.jpg').attr('alt','Add an album here').addClass('rounded');
+      let $columnText = $('<div></div>').addClass('col');
+      let $addAlbumBtn = $('<button></button>').attr('type','button').attr('id','addAlbumBtn').addClass('btn btn-success float-end').attr('data-bs-toggle','modal').attr('data-bs-target','#searchAlbum').text('+');
+      $rowActual.append($column);
+      $column.append($rowInner);
+      $rowInner.append($border);
+      $border.append($rowUnderBorder);
+      $rowUnderBorder.append($columnImg);
+      $rowUnderBorder.append($columnText);
+      $columnImg.append($placeholder);
+      $columnText.append('<ul></ul>').append('<li>test</li>');
+      $border.append($addAlbumBtn);
+    }
+    $rowActual.append($addRow);
   });
 }
+
+addRow();
