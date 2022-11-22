c4aba60d1e0e5c65dfa33b07f61ea2654190f297
Sangyeon Pak <sangyeonpak1994@gmail.com>
2022-11-22 00:43:33 -0500

    I think I'm done here. I'm gonna come back and try to implement the APIs and such

--- shelf.js
+++ shelf.js
@@ -1,13 +1,21 @@
-let $addRow = $('#addRow');
-let $container = $('.container');
-let $addAlbumBtn = $('#addAlbumBtn');
-
+//================================================== add row button on the bottom to put more shelves ============================================//
+let $addRow = $('#addRow'); // call button to add rows
+let $container = $('.container'); // call container
+let $addAlbumBtn = $('#addAlbumBtn'); // call button to add albums (non-functional right now)
 
 function addRow(){
   $addRow.click(()=>{
+
+    // get rid of the add row button from the bottom (for now)
     $addRow.detach();
+
+    // create the main row (shelf) div
     let $rowActual = $('<div></div>').addClass('row').attr('id', 'addedRow');
+
+    // append the row to container div
     $container.append($rowActual);
+
+    // creating the same column structure like in the HTML 3 times within the main row div
     for (var i = 0; i < 3; i++){
       let $column = $('<div></div>').addClass('col');
       let $rowInner = $('<div></div>').addClass('row');
@@ -27,8 +35,24 @@ function addRow(){
       $columnText.append('<ul></ul>').append('<li>test</li>');
       $border.append($addAlbumBtn);
     }
+
+    // re-attach the add row button to the bottom after creating the row div after the for loop
     $rowActual.append($addRow);
   });
 }
 
 addRow();
+//================================================================================================================================================//
+
+
+
+
+//======================================================= searching for albums (WIP) =============================================================//
+$searchButton = $('searchButton');
+$searchBar = $('#searchBar');
+
+function searchAlbums(){ // will continue after i learn more on how to use Spotify's api
+  $searchBar.click(()=>{
+    $searchBar.val();
+  })
+}
