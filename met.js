a1e5c7237e48383665547417e81026d6ed3c8335
Sangyeon Pak <sangyeonpak1994@gmail.com>
2022-11-26 00:57:11 -0500

    nevermind, i fixed it LOL. i  was working on this for a long time. got it by using .detach()

--- met.js
+++ met.js
@@ -104,14 +104,17 @@ function searchArtwork(ID){
           let $imageContainer = $(`#imageContainer${ID}`);
           function appendArt(ID){
             $appendArtBtn.click(()=>{
-// empty previous image, modal, and what you searched
+// save addArtworkBtn empty previous image, modal, and what you searched
+
+              let $addArtworkBtn = $(`#${ID}`);
+              $addArtworkBtn.detach();
               $imageContainer.empty();
               $modalResults.empty();
               $searchBar.val('');
 
 // the appending part
               $imageContainer.append($resultImage);
-              $imageContainer.append(`<button type="button" class="addArtworkBtn btn fw-bold" id="${ID}" data-bs-toggle="modal" data-bs-target="#searchArt">+</button>`);
+              $imageContainer.append($addArtworkBtn);
               $imageContainer.append(`<button class="notSeen btn" id="notSeen${ID}"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
               <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
               <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
